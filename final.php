e367b1cbb142bf1d07568d6e381dd86c1019c088
milovan-minic <milovan.minic@gmail.com>
2015-09-21 19:17:20 +0200

    Zavrsen drugi video kursa.
    Dodati fajlovi add.php, database.php, final.php, process.php i modifikovan fajl css/style.php

---
+++ final.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>PHP Quizzer</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+</head>
+
+<body>
+<header>
+    <div class="container">
+        <h1>PHP Quizzer!</h1>
+    </div>
+</header>
+
+<main>
+    <div class="container">
+        <h2>You're done!</h2>
+            <p>Congrats! You have completed the test.</p>
+            <p>Final score: 5</p>
+            <a href="question.php?n=1" class="start">Take again</a>
+    </div>
+</main>
+
+<footer>
+    <div class="container">
+        Copyright &copy; 2015 PHP Quizzer
+    </div>
+</footer>
+</body>
+</html>
